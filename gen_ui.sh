acf130eb2c0c3e07467b2460b9398aeda44f6ef4
Sam Gratrix <sam@gratrix.net>
2017-11-10 20:30:18 +0000

    Improve UI generation script.

---
+++ gen_ui.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for f in build/res/*.svg
+do
+	b=$(basename "$f")
+	r=${b%.*}
+	"/c/Program Files/Inkscape/Inkscape.exe" $f --export-png=res/${r}.png
+	"/c/Program Files/Inkscape/Inkscape.exe" $f --export-plain-svg=res/${r}.svg --export-text-to-path
+done
