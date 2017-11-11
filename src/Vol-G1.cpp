e6ee625d48a229189da4035df4f77a49a8a6f211
Sam Gratrix <sam@gratrix.net>
2017-11-11 19:56:43 +0000

    Generally improve volume module:, now 9 holes, remove knob, more lights.

--- src/Vol-G1.cpp
+++ src/Vol-G1.cpp
@@ -6,7 +6,6 @@
 struct Vol_G1 : Module
 {
 	enum ParamIds {
-		SCALE_PARAM,
 		NUM_PARAMS
 	};
 	enum InputIds {
@@ -17,7 +16,7 @@ struct Vol_G1 : Module
 		NUM_OUTPUTS,
 	};
 	enum LightIds {
-		NUM_LIGHTS = 5  // N
+		NUM_LIGHTS = 10  // N
 	};
 
 	Vol_G1()
@@ -33,18 +32,16 @@ struct Vol_G1 : Module
 
 	void step() override
 	{
-		float scale = params[SCALE_PARAM].value;
-
 		for (std::size_t i=0; i<GTX__N; ++i)
 		{
 			float input = inputs[imap(IN1_INPUT, i)].active ? inputs[imap(IN1_INPUT, i)].value : inputs[imap(IN1_INPUT, GTX__N)].value;
-			float dB    = logf(fabsf(scale * input * 0.1f)) * (10.0f / logf(20.0f));
+			float dB    = logf(fabsf(input * 0.1f)) * (10.0f / logf(20.0f));
 
 			for (int j = 0; j < NUM_LIGHTS; j++)
 			{
 				float b = clampf(dB / 3.0f + 1.0f + j, 0.0f, 1.0f);
 
-				lights[NUM_LIGHTS * i + j].setBrightnessSmooth(b);
+				lights[NUM_LIGHTS * i + j].setBrightnessSmooth(b * 0.9f);
 			}
 		}
 	}
@@ -59,15 +56,14 @@ Vol_G1_Widget::Vol_G1_Widget()
 
 	Vol_G1 *module = new Vol_G1();
 	setModule(module);
-	box.size = Vec(12*15, 380);
+	box.size = Vec(9*15, 380);
 
 	#if GTX__SAVE_SVG
 	{
-		PanelGen pg(assetPlugin(plugin, "build/res/Vol-G1.svg"), box.size, "VOL - G1");
+		PanelGen pg(assetPlugin(plugin, "build/res/Vol-G1.svg"), box.size, "VU-G1");
 
-		pg.nob_big(0, 0, "SCALE");
-		pg.bus_inx(0, 1, "VOL");
-		pg.bus_in (0, 2, "IN");
+		pg.nob_big(0.25, 0, "VOLUME");
+		pg.bus_in (0.25, 2, "IN");
 	}
 	#endif
 
@@ -89,20 +85,24 @@ Vol_G1_Widget::Vol_G1_Widget()
 	addChild(createScrew<ScrewSilver>(Vec(15, 365)));
 	addChild(createScrew<ScrewSilver>(Vec(box.size.x-30, 365)));
 
-	addParam(createParam<RoundHugeBlackKnob>(n_b(fx(0), fy(0)), module, Vol_G1::SCALE_PARAM, 0.0, 2.0, 1.0));
-
 	for (std::size_t i=0; i<GTX__N; ++i)
 	{
-		addInput(createInput<PJ301MPort>(prt(px(0, i), py(2, i)), module, Vol_G1::imap(Vol_G1::IN1_INPUT, i)));
+		addInput(createInput<PJ301MPort>(prt(px(0.25, i), py(2, i)), module, Vol_G1::imap(Vol_G1::IN1_INPUT, i)));
 	}
 
-	addInput(createInput<PJ301MPort>(prt(gx(0), gy(2)), module, Vol_G1::imap(Vol_G1::IN1_INPUT, GTX__N)));
+	addInput(createInput<PJ301MPort>(prt(gx(0.25), gy(2)), module, Vol_G1::imap(Vol_G1::IN1_INPUT, GTX__N)));
 
 	for (std::size_t i=0, k=0; i<GTX__N; ++i)
 	{
 		for (std::size_t j=0; j<Vol_G1::NUM_LIGHTS; ++j, ++k)
 		{
-			addChild(createLight<SmallLight<RedLight>>(led(gx(0)+(i-2.5f)*10.0f, gy(1)+(j-2.0f)*12.0f), module, k));
+			switch (j)
+			{
+				case 0  : addChild(createLight<SmallLight<   RedLight>>(led(gx(0.25)+(i-2.5f)*16.0f, gy(0.5)+(j-4.5f)*11.0f), module, k)); break;
+				case 1  :
+				case 2  : addChild(createLight<SmallLight<YellowLight>>(led(gx(0.25)+(i-2.5f)*16.0f, gy(0.5)+(j-4.5f)*11.0f), module, k)); break;
+				default : addChild(createLight<SmallLight< GreenLight>>(led(gx(0.25)+(i-2.5f)*16.0f, gy(0.5)+(j-4.5f)*11.0f), module, k)); break;
+			}
 		}
 	}
 }
