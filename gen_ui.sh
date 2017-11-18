e1259f92398f4f5f4e8eb295ac95f240f435c138
Sam Gratrix <sam@gratrix.net>
2017-11-18 17:23:41 +0000

    Remove PNG resources as these are now defunct.

--- gen_ui.sh
+++ gen_ui.sh
@@ -4,7 +4,7 @@ for f in build/res/*.svg
 do
 	b=$(basename "$f")
 	r=${b%.*}
-	"/c/Program Files/Inkscape/Inkscape.exe" $f --export-png=res/${r}.png
+#	"/c/Program Files/Inkscape/Inkscape.exe" $f --export-png=res/${r}.png
 	"/c/Program Files/Inkscape/Inkscape.exe" $f --export-plain-svg=res/${r}.svg --export-text-to-path
 
 	if [[ "$OSTYPE" == "msys" ]]; then
